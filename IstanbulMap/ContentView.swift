c1f5e60780993f13dcca97c10dd3312f64acdd31
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-26 00:58:52 +0300

    Initial Commit

---
+++ IstanbulMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IstanbulMap
+//
+//  Created by Abdelrahman Mohamed on 26.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IstanbulMap/IstanbulMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  IstanbulMapApp.swift
+//  IstanbulMap
+//
+//  Created by Abdelrahman Mohamed on 26.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct IstanbulMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
